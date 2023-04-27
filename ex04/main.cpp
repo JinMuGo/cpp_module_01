e0b2101c1bda813a0c3a3f902c798b82935cdab9
jgo-42 <jingmuio@gmail.com>
2023-04-27 15:01:06 +0900

    :tada: Feat: ex04 Comp.

---
+++ ex04/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 13:23:01 by jgo               #+#    #+#             */
+/*   Updated: 2023/04/27 14:51:19 by jgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+static bool	prt_err(std::string msg)
+{
+	std::cerr << msg;
+	return (EXIT_FAILURE);
+}
+
+static std::string makeReplaceFileName(std::string str) {
+	return (str.append(".replace"));
+};
+
+int main(int ac, char **av) {
+	if (ac != 4)
+		return (prt_err(ERR_ARGS));
+	const size_t s1Size = strlen(av[S1]);
+	const size_t s2Size = strlen(av[S2]);
+	std::ifstream file(av[FILE_NAME], std::ios::in);
+	std::ofstream outFile(makeReplaceFileName(av[FILE_NAME]),
+						  std::ios::trunc | std::ios::out);
+	std::string line;
+
+	if (!file.is_open() || !outFile.is_open())
+		return (prt_err(ERR_FILE));
+	while (getline(file, line)) {
+		size_t pos = line.find(av[S1]);
+		while (pos != std::string::npos) {
+			line.erase(pos, s1Size);
+			line.insert(pos, av[S2], s2Size);
+			pos = line.find(av[S1], pos + 1);
+		}
+		outFile << line << "\n";
+	}
+	file.close();
+	outFile.close();
+	return (EXIT_SUCCESS);
+}
---
+++ ex04/main.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 13:25:42 by jgo               #+#    #+#             */
+/*   Updated: 2023/04/27 14:51:24 by jgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <iostream>
+#include <fstream>
+
+# define ERR_ARGS "❌ Incorrect number of arguments\n"
+# define ERR_FILE "❌ Invalid file name\n"
+
+enum e_args {
+	EXEC_NAME,
+	FILE_NAME,
+	S1,
+	S2
+};
+
+#endif
