ee508eef39e429ef2cdea37f5cac2d3c64b51da7
jgo-42 <jingmuio@gmail.com>
2023-05-09 11:42:39 +0900

    :bug: Fix: Replace.cpp not working

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -6,11 +6,16 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 17:46:12 by jgo               #+#    #+#             */
-/*   Updated: 2023/05/08 20:47:33 by jgo              ###   ########.fr       */
+/*   Updated: 2023/05/09 11:41:50 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Replace.hpp"
+
+Replace::Replace(const char* fileName, const char* s1, const char* s2)
+	: fileName_(fileName), s1_(s1), s2_(s2) {}
+
+Replace::~Replace() {}
 
 bool Replace::prtErr(std::string msg) {
 	std::cerr << msg;
@@ -28,7 +33,7 @@ bool Replace::replaceFile(void) {
 	const size_t s2Size = this->s2_.length();
 	std::stringstream ss;
 	std::string tmp;
-	size_t pos;
+	std::size_t pos;
 
 	ss << inFile.rdbuf();
 	tmp = ss.str();
@@ -36,13 +41,8 @@ bool Replace::replaceFile(void) {
 	while (pos != std::string::npos) {
 		tmp.erase(pos, s1Size);
 		tmp.insert(pos, this->s2_);
-		pos = tmp.find(this->s2_, pos + s2Size);
+		pos = tmp.find(this->s1_, pos + s2Size);
 	}
-	outFile << tmp << "\n";
+	outFile << tmp;
 	return (0);
 }
-
-Replace::Replace(const char* fileName, const char* s1, const char* s2)
-	: fileName_(fileName), s1_(s1), s2_(s2) {}
-
-Replace::~Replace() {}
